d200fc915daa690b7766c632117ece39ee66a2f2
NatsumiUIX <velocicraft2088@gmail.com>
2020-10-18 18:26:18 -0300

    Added web protocol customization

---
+++ Connector/InvalidWebProtocolException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeReactor.CRGameJolt.Connector
+{
+    /// <summary>
+    /// Represent a not supported or invalid web protocol
+    /// </summary>
+    public class InvalidWebProtocolException : Exception
+    {
+        public InvalidWebProtocolException() : base() { }
+        public InvalidWebProtocolException(string message) : base(message) { }
+        public InvalidWebProtocolException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ Connector/WebProtocol.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeReactor.CRGameJolt.Connector
+{
+    /// <summary>
+    /// Setup a web protocol to WebCaller and URLConstructor
+    /// </summary>
+    public enum WebProtocol
+    {
+        /// <value>
+        /// Say to URLConstructor use http:// protocol
+        /// </value>
+        HTTP,
+        /// <value>
+        /// Say to URLConstructor use https:// protocol
+        /// </value>
+        HTTPS
+    }
+}
