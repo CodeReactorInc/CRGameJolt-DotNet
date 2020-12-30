7923b712d373bc17bda91d05ccd453f15d751a11
NatsumiUIX <velocicraft2088@gmail.com>
2020-12-30 13:44:46 -0300

    TableManager documentation

--- Scores/TableManager.cs
+++ Scores/TableManager.cs
@@ -4,8 +4,18 @@ using System.Xml.Linq;
 
 namespace CodeReactor.CRGameJolt.Scores
 {
+    /// <summary>
+    /// Get and manage all score tables from a game in GameJolt Game API
+    /// </summary>
+    /// <seealso cref="ScoreTable"/>
+    /// <seealso cref="ScoreValue"/>
+    /// <seealso cref="Connector.WebCaller"/>
+    /// <seealso cref="IGJObject"/>
     public class TableManager : IGJObject
     {
+        /// <param name="tableid">Id of the table that you want get</param>
+        /// <returns>The table or if doesn't exists, null</returns>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public ScoreTable this[int tableid]
         {
             get
@@ -13,7 +23,7 @@ namespace CodeReactor.CRGameJolt.Scores
                 XElement response = WebCaller.GetAsXML("scores/tables", new string[] { }).Element("response");
                 if (response.Element("success").Value != "true") throw new GameJoltAPIException(response.Element("message").Value);
                 ScoreTable result = null;
-                foreach(XElement table in response.Element("tables").Elements("table"))
+                foreach (XElement table in response.Element("tables").Elements("table"))
                 {
                     if (table.Element("id").Value == tableid.ToString())
                     {
@@ -25,6 +35,10 @@ namespace CodeReactor.CRGameJolt.Scores
             }
         }
 
+        /// <value>
+        /// Get and download all tables from GameJolt Game API
+        /// </value>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public ScoreTable[] Tables
         {
             get
@@ -40,6 +54,10 @@ namespace CodeReactor.CRGameJolt.Scores
             }
         }
 
+        /// <value>
+        /// Only get the primary table from GameJolt Game API
+        /// </value>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public ScoreTable Primary
         {
             get
@@ -59,6 +77,11 @@ namespace CodeReactor.CRGameJolt.Scores
             }
         }
 
+        /// <summary>
+        /// Initialize table manager and download a test purpose list
+        /// </summary>
+        /// <param name="webCaller">A instance of <see cref="WebCaller"/> to download the data</param>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public TableManager(WebCaller webCaller)
         {
             WebCaller = webCaller;
@@ -66,6 +89,12 @@ namespace CodeReactor.CRGameJolt.Scores
             if (response.Element("success").Value != "true") throw new GameJoltAPIException(response.Element("message").Value);
         }
 
+        /// <summary>
+        /// Get the table name using the <paramref name="tableid"/>
+        /// </summary>
+        /// <param name="tableid">Table id used as reference</param>
+        /// <returns>The name of table or null if doesnt exists</returns>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public string GetName(int tableid)
         {
             XElement response = WebCaller.GetAsXML("scores/tables", new string[] { }).Element("response");
@@ -82,6 +111,12 @@ namespace CodeReactor.CRGameJolt.Scores
             return result;
         }
 
+        /// <summary>
+        /// Get the table description using the <paramref name="tableid"/>
+        /// </summary>
+        /// <param name="tableid">Table id used as reference</param>
+        /// <returns>The name of table or null if doesnt exists</returns>
+        /// <exception cref="GameJoltAPIException">Throwed if GameJolt Game API return a non-success response</exception>
         public string GetDescription(int tableid)
         {
             XElement response = WebCaller.GetAsXML("scores/tables", new string[] { }).Element("response");
@@ -98,8 +133,10 @@ namespace CodeReactor.CRGameJolt.Scores
             return result;
         }
 
+        /// <inheritdoc/>
         public WebCaller WebCaller { get; set; }
 
+        /// <inheritdoc/>
         public void Update()
         {
             XElement response = WebCaller.GetAsXML("scores/tables", new string[] { }).Element("response");
