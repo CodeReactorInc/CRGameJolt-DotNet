f7940960b9306056dd99b3735ae037759866f943
NatsumiUIX <velocicraft2088@gmail.com>
2020-12-30 10:28:05 -0300

    ScoreTable template

--- Scores/ScoreTable.cs
+++ Scores/ScoreTable.cs
@@ -1,7 +1,35 @@
-﻿namespace CodeReactor.CRGameJolt.Scores
+﻿using CodeReactor.CRGameJolt.Connector;
+using CodeReactor.CRGameJolt.Users;
+
+namespace CodeReactor.CRGameJolt.Scores
 {
-    public class ScoreTable
+    public class ScoreTable : IGJObject
     {
         public int Id { get; private set; }
+
+        public ScoreTable(int tableid, WebCaller webcaller) { }
+
+        public ScoreValue Fetch(GameJoltMe user) { }
+
+        public ScoreValue Fetch(string guest) { }
+
+        public ScoreValue[] Fetch(int limit) { }
+
+        public ScoreValue[] BetterThan(int sort, int limit) { }
+
+        public ScoreValue[] WorseThan(int sort, int limit) { }
+
+        public void Add(string score, int sort, string extradata, GameJoltMe user) { }
+
+        public void Add(string score, int sort, string extradata, string guest) { }
+
+        public int GetRank(int sort) { }
+
+        public WebCaller WebCaller { get; set; }
+
+        public void Update()
+        {
+            
+        }
     }
 }
