ad43eea73ef73d285dc9652ec866cc6f12a2640e
NatsumiUIX <velocicraft2088@gmail.com>
2020-12-30 12:16:29 -0300

    TableManager template

--- Scores/TableManager.cs
+++ Scores/TableManager.cs
@@ -1,6 +1,53 @@
-﻿namespace CodeReactor.CRGameJolt.Scores
+﻿using CodeReactor.CRGameJolt.Connector;
+
+namespace CodeReactor.CRGameJolt.Scores
 {
-    class TableManager
+    public class TableManager : IGJObject
     {
+        public ScoreTable this[int tableid]
+        {
+            get
+            {
+
+            }
+        }
+
+        public ScoreTable[] Tables
+        {
+            get
+            {
+
+            }
+        }
+
+        public ScoreTable Primary
+        {
+            get
+            {
+
+            }
+        }
+
+        public TableManager(WebCaller webCaller)
+        {
+
+        }
+
+        public string GetName(int tableid)
+        {
+
+        }
+
+        public string GetDescription(int tableid)
+        {
+
+        }
+
+        public WebCaller WebCaller { get; set; }
+
+        public void Update()
+        {
+
+        }
     }
 }
