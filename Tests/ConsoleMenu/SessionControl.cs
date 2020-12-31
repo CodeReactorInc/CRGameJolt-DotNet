5a896c93403f37f1c538559aa8ddf949304e620b
NatsumiUIX <velocicraft2088@gmail.com>
2020-12-31 17:59:24 -0300

    SessionControl functional

---
+++ Tests/ConsoleMenu/SessionControl.cs
@@ -0,0 +1,134 @@
+﻿using CodeReactor.CRGameJolt.Users;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeReactor.CRGameJolt.Test.ConsoleMenu
+{
+    public static class SessionControl
+    {
+        public static void Collect()
+        {
+            Console.WriteLine("Getting or creating a instance for SessionManager");
+            SessionManager manager = MainMenu.Instance.Memory.GameJolt.UserLogged.Session;
+            Console.WriteLine("Checking atual state...");
+            if (manager.Check())
+            {
+                Console.WriteLine("1. Set active");
+                Console.WriteLine("2. Set idle");
+                Console.WriteLine("3. Enable/Disable Autoping");
+                Console.WriteLine("4. Close");
+                Console.WriteLine("5. Ping");
+                Console.WriteLine("6. Back");
+                Console.Write("Select one option: ");
+                try
+                {
+                    int o = int.Parse(Console.ReadLine());
+
+                    switch (o)
+                    {
+                        case 1:
+                            manager.Status = SessionStatus.Active;
+                            Console.WriteLine("Status configured");
+                            Collect();
+                            break;
+                        case 2:
+                            manager.Status = SessionStatus.Idle;
+                            Console.WriteLine("Status configured");
+                            Collect();
+                            break;
+                        case 3:
+                            if (MainMenu.Instance.Memory.Autoping)
+                            {
+                                MainMenu.Instance.Memory.Autoping = false;
+                                if (MainMenu.Instance.Memory.AutopingThread != null)
+                                {
+                                    try
+                                    {
+                                        MainMenu.Instance.Memory.AutopingThread.Abort();
+                                    } catch (PlatformNotSupportedException) { }
+                                }
+                                MainMenu.Instance.Memory.AutopingThread = null;
+                                Console.WriteLine("Autoping disabled");
+                                Collect();
+                            } else
+                            {
+                                MainMenu.Instance.Memory.Autoping = true;
+                                MainMenu.Instance.Memory.AutopingThread = new Thread(Autoping);
+                                MainMenu.Instance.Memory.AutopingThread.Name = "Autoping Thread";
+                                MainMenu.Instance.Memory.AutopingThread.Start(MainMenu.Instance.Memory);
+                                Console.WriteLine("Autoping enabled");
+                                Collect();
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Closing session...");
+                            manager.Close();
+                            Collect();
+                            break;
+                        case 5:
+                            Console.WriteLine("Manual ping...");
+                            manager.Ping();
+                            Collect();
+                            break;
+                        case 6:
+                            MainMenu.Instance.Start();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid number, try again");
+                            Collect();
+                            break;
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number, try again");
+                    Collect();
+                }
+            } else
+            {
+                Console.WriteLine("1. Open session");
+                Console.WriteLine("2. Back");
+                Console.Write("Select one option: ");
+                try
+                {
+                    int o = int.Parse(Console.ReadLine());
+
+                    switch (o)
+                    {
+                        case 1:
+                            manager.Open();
+                            Console.WriteLine("Opened session");
+                            Collect();
+                            break;
+                        case 2:
+                            MainMenu.Instance.Start();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid number, try again");
+                            Collect();
+                            break;
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number, try again");
+                    Collect();
+                }
+            }
+        }
+
+        public static void Autoping(object data)
+        {
+            CentralMemory memory = (CentralMemory)data;
+            while (memory.Autoping)
+            {
+                memory.GameJolt.UserLogged.Session.Ping();
+                Thread.Sleep((int)SessionManager.Timeout);
+            }
+        }
+    }
+}
