a393135faf9cd3613f396bc92cb0aac495c2e85c
NatsumiUIX <velocicraft2088@gmail.com>
2020-12-31 17:58:21 -0300

    GameLogin functional

---
+++ Tests/ConsoleMenu/GameLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeReactor.CRGameJolt.Test.ConsoleMenu
+{
+
+    public class GameLogin
+    {
+        public string GameId { get; private set; }
+        public string GameKey { get; private set; }
+
+        public void Collect()
+        {
+            Console.Write("Game ID: ");
+            GameId = Console.ReadLine();
+            Console.Write("Game Key: ");
+            GameKey = Console.ReadLine();
+        }
+    }
+}
