22a151a6a28fc6631442e5f9048707fe6d9b63d1
NatsumiUIX <velocicraft2088@gmail.com>
2021-01-01 17:10:54 -0300

    FriendProcessor functional

--- Tests/ConsoleMenu/FriendProcessor.cs
+++ Tests/ConsoleMenu/FriendProcessor.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CodeReactor.CRGameJolt.Users;
+using System;
 
 namespace CodeReactor.CRGameJolt.Test.ConsoleMenu
 {
-    class FriendProcessor
+    public static class FriendProcessor
     {
+        public static void Collect()
+        {
+            Console.WriteLine("Updating or creating a instance of FriendList...");
+
+            MainMenu.Instance.Memory.GameJolt.UserLogged.Friends.Update();
+
+            foreach (GameJoltUser user in MainMenu.Instance.Memory.GameJolt.UserLogged.Friends.Values)
+            {
+                UserInfo.ShowInfo(user);
+            }
+
+            MainMenu.Instance.Start();
+        }
     }
 }
